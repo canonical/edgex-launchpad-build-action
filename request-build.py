cba0df5718509ee7238bc4ccd0e4dd6585502239
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2022-07-11 16:56:35 +0200

    Formatting

--- request-build.py
+++ request-build.py
@@ -8,15 +8,19 @@ ubuntu_series = os.environ['UBUNTU_SERIES']
 snap_name = os.environ['SNAP_NAME']
 arch = os.environ['ARCH']
 
-launchpad = Launchpad.login(consumer_name, access_token,
-                            access_secret, service_root='production', version='devel')
+launchpad = Launchpad.login(consumer_name, access_token, access_secret,
+                            service_root='production', version='devel')
 
 ubuntu = launchpad.distributions["ubuntu"]
 release = ubuntu.getSeries(name_or_version=ubuntu_series)
 arch = release.getDistroArchSeries(archtag=arch)
 
-snap = launchpad.snaps.getByName(
-    name=snap_name, owner=launchpad.people["canonical-edgex"])
-request = snap.requestBuild(archive=ubuntu.main_archive, distro_arch_series=arch,
-                            pocket='Updates', channels={"snapcraft": "latest/stable"})
+snap = launchpad.snaps.getByName(name=snap_name,
+                                 owner=launchpad.people["canonical-edgex"])
+
+request = snap.requestBuild(archive=ubuntu.main_archive,
+                            distro_arch_series=arch,
+                            pocket='Updates',
+                            channels={"snapcraft": "latest/stable"})  # launchpad uses latest/stable/launchpad-buildd channel by default
+
 print("Build request has been submitted: {0}".format(request))
